d6200d7a27cd94e97753758a87f53e326717076c
Brian Hicks <brian@brianthicks.com>
2016-03-29 14:29:04 -0500

    install: install funcs

--- install.zsh
+++ install.zsh
@@ -1,17 +1,34 @@
 #!/usr/bin/env zsh
 fpath=($PWD/apps/zish/.zsh.funcs)
 
-autoload unstow-app brew-install
+autoload unstow-app brew-install directory
 
 # basic utilities needed for the rest of the installation
 brew-install stow
 
+# system
+directory ~/code
+
 for app in apps/*; do
     echo
     echo "### $(basename $app) ###"
+
+    # run installation script
     if [ -f $app/install.zsh ]; then
         eval "$(cat $app/install.zsh)"
     else
         echo "app does not have install.sh, skipping"
+        continue
+    fi
+
+    # install funcs
+    if [ -d $app/.zsh.funcs ]; then
+        fname=~/.zshrc.d/$(basename $app)-funcs.zsh
+        echo "#!/usr/bin/env zsh" > $fname
+        for func in $app/.zsh.funcs/*; do
+            echo "autoload $(basename $func)" >> $fname
+        done
+
+        echo "installed funcs for $(basename $app)"
     fi
 done
